69681a07c68af12a6665eb1ddbb3b6082cbc973b
schfenton <schfenton@gmail.com>
2022-02-11 23:27:46 -0800

    Added logo to navbar and hid navlinks

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -5,8 +5,9 @@ import {
   Route,
   Link,
   Outlet,
-  Navigate,
+  Navigate
 } from "react-router-dom";
+import badge from "./assets/logo32.png";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
@@ -53,21 +54,20 @@ function NavBar() {
     <>
       <div class="container">
         <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-          <a
-            href="/"
+          <Link
+            to="/"
             class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
           >
-            <svg
+            <img
+              src = {badge}
               class="bi me-2"
-              width="40"
+              width="32"
               height="32"
-              role="img"
               aria-label="Bootstrap"
-              hidden
-            ></svg>
-          </a>
+            />
+          </Link>
 
-          <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0" hidden>
             <li>
               <Link to="/" class="nav-link px-2 link-secondary">
                 Home
