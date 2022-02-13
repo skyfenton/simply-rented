dd23609c776873d56e35bacee193cf50c7da28fc
schfenton <schfenton@gmail.com>
2022-02-12 17:37:29 -0800

    Logo and login now stay opposite sides of navbar

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -56,7 +56,7 @@ function NavBar() {
         <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
           <Link
             to="/"
-            class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
+            class="d-flex align-items-center me-auto mb-2 mb-md-0 text-dark text-decoration-none"
           >
             <img
               src = {badge}
@@ -74,7 +74,7 @@ function NavBar() {
               </Link>
             </li>
             <li>
-              <a href="#" class="nav-link px-2 link-dark">
+              <a href="/about" class="nav-link px-2 link-dark">
                 About
               </a>
             </li>
