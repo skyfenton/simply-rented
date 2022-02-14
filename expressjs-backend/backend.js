8affe8d9e1cf725296d66174517f67bf07dbb827
ZachLofquist <zlofquist@gmail.com>
2022-02-14 13:31:34 -0800

    Linked Backend to Database

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -3,42 +3,11 @@ const cors = require('cors');
 const app = express(); //make express instance
 const port = 5000; //constant to listen on port 5000
 
-const users = { 
-	users_list :
-	[]
-}
+const userServices = require('./models/user-services');
+
 
 app.use(express.json()); //process in json format
 app.use(cors());
-
-// For sending users to database
-// var mongoose = require("mongoose");
-// mongoose.Promise = global.Promise;mongoose.connect("mongodb://localhost:27017/simply-rented");
-
-// var bodyParser = require('body-parser');
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// var nameSchema = new mongoose.Schema({
-//   firstName: String,
-//   lastName: String,
-//   email: String,
-//   username: String,
-//   password: String
-//  });
-
-// var User = mongoose.model("User", nameSchema);
-
-// app.post("/addedUser", (req, res) => {
-//   var myData = new User(req.body);
-//  myData.save()
-//  .then(item => {
-//  res.send("item saved to database");
-//  })
-//  .catch(err => {
-//  res.status(400).send("unable to save to database");
-//  });
-// });
 
 // setup get API endpoint to match url pattern '/' (root) and two json objects:
 // req for incoming, res for outgoing response
@@ -56,19 +25,28 @@ app.post('/login', (req, res) => {
   }
 })
 
-app.get('/users', (req, res) => {
-	 res.send(users);
+
+app.get('/users', async (req, res) => {
+  const name = req.query['firstName'];
+  try {
+      const result = await userServices.getUsers(name);
+      res.send({users_list: result});         
+  } catch (error) {
+      console.log(error);
+      res.status(500).send('An error ocurred in the server.');
+  }
 });
+
 
 app.post('/signup', (req, res) => {
   const userToAdd = req.body;
-  addUser(userToAdd);
-  res.status(201).send(userToAdd);
+  const savedUser = userServices.addUser(userToAdd);
+  if (savedUser)
+    res.status(201).send(savedUser);
+  else
+    res.status(500).end();
 });
 
-function addUser(user){
-  users['users_list'].push(user);
-}
 
 // make app listen to requests at port number
 app.listen(port, () => {
