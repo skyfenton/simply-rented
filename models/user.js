e1feeae3395370cb09d1b8102010d1fb4503b758
ZachLofquist <zlofquist@gmail.com>
2022-02-13 16:22:01 -0800

    Added Databsae Models

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  zip_code: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  listings: {
+    type: Object,
+    required: true,
+    trim: true,
+  },
+  rentals: {
+    type: Object,
+    required: true,
+    trim: true,
+  },
+}, { collection: 'users_list' });
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
