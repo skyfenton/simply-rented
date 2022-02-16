8c7786a17df4932f506d914cc4318a03e95ec305
ZachLofquist <zlofquist@gmail.com>
2022-02-15 19:54:02 -0800

    Formatted backend and removed excess

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -1,10 +1,10 @@
 const express = require("express"); // import express
 const cors = require("cors");
 
-const userServices = require('./models/user-services');
+const userServices = require("./models/user-services");
 
 const app = express();
-const port = 5000; 
+const port = 5000;
 
 app.use(express.json()); // process in json format
 app.use(cors());
@@ -15,53 +15,38 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-function verifyUser(email, password) {                // Need to update further as this isn't checking password
+function verifyUser(email) {
+  // Need to update further as this isn't checking password!!
   const user = userServices.getUsers(email);
-  if (user)
-    return true;
-  else
-    return false;
+  if (user) return true;
+  return false;
 }
 
 // Verify login info with backend (right now just sends 200 if fields exist)
 app.post("/login", (req, res) => {
   const { body } = req;
-  if (body.email && body.password && verifyUser(body.email, body.password)) {
+  if (body.email && body.password && verifyUser(body.email)) {
     res.status(200).end();
   } else {
     res.status(400).end();
   }
-})
+});
 
-
-app.get('/users', async (req, res) => {
-  const name = req.query['firstName'];
+app.get("/users", async (req, res) => {
+  const name = req.query.firstName;
   try {
-      const result = await userServices.getUsers(name);
-      res.send({users_list: result});         
+    const result = await userServices.getUsers(name);
+    res.send({ users_list: result });
   } catch (error) {
-      console.log(error);
-      res.status(500).send('An error ocurred in the server.');
+    res.status(500).send("An error occurred in the server.");
   }
 });
 
-
-app.post('/signup', (req, res) => {
-  const userToAdd = req.body;
-  const savedUser = userServices.addUser(userToAdd);
-  if (savedUser)
-    res.status(201).send(savedUser);
-  else
-    res.status(500).end();
-});
-
-
 app.post("/signup", (req, res) => {
   const userToAdd = req.body;
-  if (users.has(userToAdd.email)) {
-    res.status(200).send("email exists");
-  } else if (addUser(userToAdd)) res.status(201).end();
-  else res.status(400).end();
+  const savedUser = userServices.addUser(userToAdd);
+  if (savedUser) res.status(201).send(savedUser);
+  else res.status(500).end();
 });
 
 // make app listen to requests at port number
