634f36aace730832732897da8ef275f54d6e8113
ZachLofquist <zlofquist@gmail.com>
2022-02-23 11:37:50 -0800

    Testing some Heroku stuff

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -6,7 +6,7 @@ const userServices = require("./models/user-services");
 const app = express();
 const port = 5000;
 
-app.set("port", process.env.PORT || port)
+app.set("port", process.env.PORT || port);
 
 app.use(express.json()); // process in json format
 app.use(cors());
