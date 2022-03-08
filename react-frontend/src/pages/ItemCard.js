48a957d4039adee2793bab7c1d51e2f3f4cc662f
schfenton <schfenton@gmail.com>
2022-03-08 00:21:42 -0800

    Removed image testing

--- react-frontend/src/pages/ItemCard.js
+++ react-frontend/src/pages/ItemCard.js
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import DetailedView from "./DetailedView";
 import axios from "axios";
-import baby from "../stockImages/baby.png";
 //import surfboard from ".."
 
 async function handleClick(props) {
@@ -23,8 +22,7 @@ export default function ItemCard(props) {
     <div className="col-6 col-sm-4 col-md-3 col-xl-2">
       <div className="card mb-5 box-shadow">
         <img
-          src={baby}
-          //src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffreesvg.org%2Fimg%2FPlaceholder.png&f=1&nofb=1"
+          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffreesvg.org%2Fimg%2FPlaceholder.png&f=1&nofb=1"
           className="card-img-top"
           alt="..."
         />
